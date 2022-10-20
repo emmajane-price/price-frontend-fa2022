debf066c064886f03f4b3cf1d25d639fe57a41cd
Emma Price <ejprice54321@gmail.com>
2022-10-19 23:04:17 -0500

    assign concat correctly

--- module7-solution/js/app.js
+++ module7-solution/js/app.js
@@ -37,7 +37,7 @@ function ShoppingListCheckOffService() {
 
     service.moveItem = function(itemIndex) {
         var boughtItem = toBuyItems.splice(itemIndex, 1);
-        alreadyBoughtItems.concat(boughtItem);
+        alreadyBoughtItems = alreadyBoughtItems.concat(boughtItem);
     }
 
     service.getToBuyItems = function () {
